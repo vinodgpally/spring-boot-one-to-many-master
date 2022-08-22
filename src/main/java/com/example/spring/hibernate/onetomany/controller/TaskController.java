7cd53db29c96e65de19a5f8eabc2a4108133e36a
Vinod Kumar Reddy Gpally <vinodkumarreddy.g@gmail.com>
2022-08-22 22:34:34 +0530

    Update TaskController.java
    Removed unnecessary commented code

--- src/main/java/com/example/spring/hibernate/onetomany/controller/TaskController.java
+++ src/main/java/com/example/spring/hibernate/onetomany/controller/TaskController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TaskController {
